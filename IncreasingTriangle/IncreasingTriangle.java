60e4228bb91ddc2c6a1b2a5c976d2abfd24525e7
Darshan kg <darshan4106@gmail.com>
2025-08-09 20:50:44 +0530

    Added IncreasingTriangle

--- IncreasingTriangle/IncreasingTriangle.java
+++ IncreasingTriangle/IncreasingTriangle.java
@@ -1,8 +1,9 @@
-public class Square {
+public class IncreasingTriangle {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                System.out.print("  *");
+            for (int j = 0; j <= i; j++) {
+                System.out.print(" * ");
+
             }
             System.out.println();
         }
